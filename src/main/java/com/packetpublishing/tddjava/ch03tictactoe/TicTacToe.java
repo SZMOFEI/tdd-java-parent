3f449843b40fc73547ead0bc727191042fd12775
mofei <mof@seirobotics.net>
2020-12-11 21:46:40 +0800

    demo

--- src/main/java/com/packetpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/packetpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -2,9 +2,75 @@ package com.packetpublishing.tddjava.ch03tictactoe;
 
 /**
  * @author mofei
- * @see tdd-java-ch03-tic-tac-toe
  * @since 2020/12/7 11:16
  */
 public class TicTacToe {
+    private char[][] board = {{'\0', '\0', '\0'}, {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
+    private char lastPlayer = '\0';
+    private final int SIZE = 3;
 
+    public String play(int x, int y) {
+        checkAxis(x);
+        checkAxis(y);
+        lastPlayer = nextPlayer();
+        setBox(x, y, lastPlayer);
+        if (isWin()) {
+            return lastPlayer + " is the Winner";
+        } else if (isLaw()) {
+            return "is Law";
+        }
+        return "No Winner";
+    }
+
+    private boolean isLaw() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int y = 0; y < SIZE; y++) {
+                if (board[i][y] == '\0') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isWin() {
+        int playTotal = lastPlayer * SIZE;
+        for (int i = 0; i < SIZE; i++) {
+            if (board[0][i] + board[1][i] + board[2][i] == playTotal) {
+                return true;
+            }
+            if (board[i][1] + board[i][1] + board[i][2] == playTotal) {
+                return true;
+            }
+        }
+        if (board[0][0] + board[1][1] + board[2][2] == playTotal) {
+            return true;
+        }
+        if (board[0][2] + board[1][1] + board[2][0] == playTotal) {
+            return true;
+        }
+        return false;
+    }
+
+    private void setBox(int x, int y, char lastPlayer) {
+        if (board[x - 1][y - 1] != '\0') {
+            throw new RuntimeException("X is occurd board");
+        } else {
+            board[x - 1][y - 1] = lastPlayer;
+        }
+    }
+
+    private void checkAxis(int x) {
+        if (x < 1 || x > 3) {
+            throw new RuntimeException("X is out board");
+        }
+    }
+
+    public char nextPlayer() {
+        if (lastPlayer == 'X') {
+
+            return 'O';
+        }
+        return 'X';
+    }
 }
--- src/test/java/com/packetpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
+++ src/test/java/com/packetpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
@@ -1,8 +1,13 @@
 package com.packetpublishing.tddjava.ch03tictactoe;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * @author mofei
@@ -10,14 +15,135 @@ import org.junit.rules.ExpectedException;
  */
 public class TicTacToeSpec {
     @Rule
-    public ExpectedException exception=ExpectedException.none();
+    public ExpectedException exception = ExpectedException.none();
 
     private TicTacToe ticTacToe;
 
     @Before
-    public final void before()   {
-        ticTacToe= new TicTacToe();
+    public final void before() {
+        ticTacToe = new TicTacToe();
     }
 
+    @Test
+    public void 如果X轴越界则运行时异常() {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(5, 2);
+    }
+
+    @Test
+    public void 如果Y轴越界则运行时异常() {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(2, 5);
+    }
+
+    @Test
+    public void 如果重复下棋则运行时异常() {
+        ticTacToe.play(1, 2);
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(1, 2);
+    }
+
+    @Test
+    public void testNextPlayer() {
+        assertEquals('X', ticTacToe.nextPlayer());
+    }
+
+    @Test
+    public void testNextPlayerIsO() {
+        ticTacToe.play(1, 2);
+        assertEquals('O', ticTacToe.nextPlayer());
+    }
+
+    @Test
+    public void whenThirdTurnThenNextPlayerO() {
+        ticTacToe.play(1, 2);
+        ticTacToe.play(2, 1);
+        assertEquals('X', ticTacToe.nextPlayer());
+    }
+
+
+    @Test
+    public void 没有获胜者() {
+        String actual = ticTacToe.play(3, 2);
+        assertEquals("No Winner", actual);
+    }
+
+    /**
+     * X X X
+     * 0 0 0
+     * 0 0 0
+     */
+    @Test
+    public void 如果水平直线一排则胜利() {
+        ticTacToe.play(1, 1);
+        ticTacToe.play(2, 2);
+        ticTacToe.play(1, 2);
+        ticTacToe.play(2, 3);
+        String actual = ticTacToe.play(1, 3);
+        assertEquals("X is the Winner", actual);
+    }
+
+    /**
+     * X 0 o
+     * X o 0
+     * X 0 0
+     */
+    @Test
+    public void 如果垂直线是一排则胜利() {
+        ticTacToe.play(1, 1);
+        ticTacToe.play(2, 2);
+        ticTacToe.play(2, 1);
+        ticTacToe.play(1, 3);
+        String actual = ticTacToe.play(3, 1);
+        assertEquals("X is the Winner", actual);
+    }
+
+    /**
+     * X 0 o
+     * 0 X o
+     * o 0 x
+     */
+    @Test
+    public void 如果是斜对角线则是O获得胜利() {
+        ticTacToe.play(1, 1); // X
+        ticTacToe.play(1, 2); // O
+        ticTacToe.play(2, 2); // X
+        ticTacToe.play(1, 3); // O
+        String actual = ticTacToe.play(3, 3); // O
+        assertEquals("X is the Winner", actual);
+    }
+    /**
+     * 0 0 X
+     * 0 X o
+     * X 0 o
+     */
+    @Test
+    public void 如果是斜对角线2则获胜的是O() {
+        ticTacToe.play(3,1);
+        ticTacToe.play(2,1);
+        ticTacToe.play(2,2);
+        ticTacToe.play(1,2);
+        String play = ticTacToe.play(1, 3);
+        assertEquals("X is the Winner", play);
+    }
+
+    /**
+     * X 0 o
+     * o o X
+     * X 0 X
+     */
+    @Test
+    public void 如果棋局走完则平局() {
+        ticTacToe.play(1,1); //X
+        ticTacToe.play(2,1); //O
+        ticTacToe.play(3,3); //X
+        ticTacToe.play(2,2); //o
+        ticTacToe.play(2,3); //x
+        ticTacToe.play(1,3); //O
+        ticTacToe.play(3,1); //X
+        ticTacToe.play(3,2); //o
+        String play = ticTacToe.play(1, 2); //x
+        assertEquals("is Law",play);
+    }
 
 }
